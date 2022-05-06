6abc4ee151b1802a6d09659298c4877c852c9396
Pedro Barreto <pbdeoliveira@gmail.com>
2022-05-06 08:29:22 -0300

    fix: phone mask

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function phoneMask (telefone) {
   return telefone.replace(/\D/g, '')
     .replace(/^(\d)/, '($1')
     .replace(/^(\(\d{2})(\d)/, '$1) $2')
-    .replace(/(\d{4})(\d{1,5})/, '$1-$2')
+    .replace(/(\d{5})(\d{1,4})/, '$1-$2')
     .replace(/(-\d{5})\d+?$/, '$1');
 }
 
@@ -22,7 +22,7 @@ function createClientCard({ nome, sobrenome, email_address, telefone, uf }) {
   section.appendChild(createCustomElement('span', 'client-name', `${nome} ${sobrenome}`));
   section.appendChild(createCustomElement('span', 'client-email', email_address));
   section.appendChild(createCustomElement('span', 'client-phone', phoneMask(telefone)));
-  section.appendChild(createCustomElement('span', 'client-state', `estado: ${uf}`));
+  section.appendChild(createCustomElement('span', 'client-state', `Estado: ${uf}`));
 
   return section;
 }
