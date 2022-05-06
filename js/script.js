a4377bbd25c8e7e2671f16085de361058a8137e6
Pedro Barreto <pbdeoliveira@gmail.com>
2022-05-06 07:03:36 -0300

    feat: createCustomElement & createClientCard function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-const addClients = document.querySelector('.client-card');
+const addClients = document.querySelector('.client-grid');
+
+function createCustomElement(element, className, innerText) {
+  const item = document.createElement(element);
+  item.className = className;
+  item.innerText = innerText;
+  return item;
+}
+
+function createClientCard({ nome, sobrenome, email_address, telefone, uf }) {
+
+  const section = document.createElement('section');
+  section.className = 'client-card';
+  section.appendChild(createCustomElement('span', 'client-name', nome + sobrenome));
+  section.appendChild(createCustomElement('span', 'client-email', email_address));
+  section.appendChild(createCustomElement('span', 'client-phone', telefone));
+  section.appendChild(createCustomElement('span', 'client-state', `estado: ${uf}`));
+
+  return section;
+}
 
 const fetchApi = () => {
   addClients.innerHTML = '<h2 class =\'loading\'>Carregando...</h2>';
   fetch(`http://localhost:3001/clients`)
    .then((response) => response.json()
    .then((dados) => {
-     addClients.innerHTML = '';
-  }));
-  };
+    addClients.innerHTML = '';
+    dados.map(({ nome, sobrenome, email_address, telefone, uf }) =>
+    addClients.appendChild(createClientCard({
+    nome, sobrenome, email_address, telefone, uf })));
+   }));
+   };
 
   window.onload = () => { 
     fetchApi();
