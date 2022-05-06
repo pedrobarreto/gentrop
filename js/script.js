dff26a8d2db15efa460853b1cf6c9e7d4aa139d0
Pedro Barreto <pbdeoliveira@gmail.com>
2022-05-06 06:52:48 -0300

    feat: client-card section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+const addClients = document.querySelector('.client-card');
+
 const fetchApi = () => {
+  addClients.innerHTML = '<h2 class =\'loading\'>Carregando...</h2>';
   fetch(`http://localhost:3001/clients`)
    .then((response) => response.json()
    .then((dados) => {
-     console.log(dados);
+     addClients.innerHTML = '';
   }));
   };
 
