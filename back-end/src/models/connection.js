6fe4bb4acd16a6863dd60485c9ad26c84efd65e1
Pedro Barreto <pbdeoliveira@gmail.com>
2022-05-06 10:10:26 -0300

    fix: connection db name

--- back-end/src/models/connection.js
+++ back-end/src/models/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'gentrop',
+    database: process.env.MYSQL_DB_NAME,
   });
    
    module.exports = connection;
