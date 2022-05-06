9a4eadf8e2e9419ad4bc43abb31292cdd551b287
Pedro Barreto <pbdeoliveira@gmail.com>
2022-05-06 10:12:20 -0300

    fix: fetch to online heroku api

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -29,7 +29,7 @@ function createClientCard({ nome, sobrenome, email_address, telefone, uf }) {
 
 const fetchApi = () => {
   addClients.innerHTML = '<h2 class =\'loading\'>Carregando...</h2>';
-  fetch(`http://localhost:3001/clients`)
+  fetch(`https://gentrop.herokuapp.com/clients`)
    .then((response) => response.json()
    .then((dados) => {
     addClients.innerHTML = '';
