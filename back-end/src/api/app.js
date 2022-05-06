4534c2fe50392216109d6811836561eba3fe21ac
Pedro Barreto <pbdeoliveira@gmail.com>
2022-05-06 09:45:43 -0300

    chore: remove cors and nodemon

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const { routes }  = require('../routes');
  
 const app = express();
-
-app.use(cors());
 
 routes.forEach((route) => {
   const { method, path, middleware, controller } = route;
